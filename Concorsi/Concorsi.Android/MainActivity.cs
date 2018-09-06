2f495afdd946cac589d8f4e0f5b5d1537afe9a14
ASvalente <antosaverio.valente@gmail.com>
2018-09-06 11:10:31 +0200

    aggiunto REST e custom render video

--- Concorsi/Concorsi.Android/MainActivity.cs
+++ Concorsi/Concorsi.Android/MainActivity.cs
@@ -1,25 +1,79 @@
-﻿using System;
-
-using Android.App;
+﻿using Android.App;
 using Android.Content.PM;
-using Android.Runtime;
 using Android.Views;
-using Android.Widget;
 using Android.OS;
+using ImageCircle.Forms.Plugin.Droid;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.Android;
+using static Concorsi.Droid.MainActivity;
+using Xfx;
+
+[assembly: ExportRenderer(typeof(WebView), typeof(ScrollableWebViewRendererAndroid))]
 
 namespace Concorsi.Droid
 {
     [Activity(Label = "Concorsi", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
     public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
     {
-        protected override void OnCreate(Bundle savedInstanceState)
+        protected override void OnCreate(Bundle bundle)
         {
             TabLayoutResource = Resource.Layout.Tabbar;
             ToolbarResource = Resource.Layout.Toolbar;
+            Window.SetFlags(WindowManagerFlags.Secure, WindowManagerFlags.Secure);
+            base.OnCreate(bundle);
 
-            base.OnCreate(savedInstanceState);
-            global::Xamarin.Forms.Forms.Init(this, savedInstanceState);
+            XfxControls.Init();
+
+            global::Xamarin.Forms.Forms.Init(this, bundle);
+            // Serve per non far andare l'applicazione in onSleep automaticamente
+            this.Window.SetFlags(WindowManagerFlags.KeepScreenOn, WindowManagerFlags.KeepScreenOn);
+            ImageCircleRenderer.Init();
             LoadApplication(new App());
+        }
+
+        public override void OnBackPressed()
+        {
+
+        }
+
+        public class ScrollableWebViewRendererAndroid : WebViewRenderer
+        {
+
+            public ScrollableWebViewRendererAndroid()
+            {
+                System.Diagnostics.Debug.WriteLine("ScrollableWebViewRendererAndroid()");
+            }
+
+            protected override void OnElementChanged(ElementChangedEventArgs<WebView> e)
+            {
+                base.OnElementChanged(e);
+
+                if (e.OldElement != null)
+                {
+                    Control.Touch -= Control_Touch;
+                }
+
+                if (e.NewElement != null)
+                {
+                    Control.Touch += Control_Touch;
+                }
+            }
+
+            void Control_Touch(object sender, Android.Views.View.TouchEventArgs e)
+            {
+                // Executing this will prevent the Scrolling to be intercepted by parent views
+                switch (e.Event.Action)
+                {
+                    case MotionEventActions.Down:
+                        Control.Parent.RequestDisallowInterceptTouchEvent(true);
+                        break;
+                    case MotionEventActions.Up:
+                        Control.Parent.RequestDisallowInterceptTouchEvent(false);
+                        break;
+                }
+                // Calling this will allow the scrolling event to be executed in the WebView
+                Control.OnTouchEvent(e.Event);
+            }
         }
     }
 }
--- Concorsi/Concorsi.iOS/AppDelegate.cs
+++ Concorsi/Concorsi.iOS/AppDelegate.cs
@@ -1,10 +1,18 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-
 using Foundation;
 using UIKit;
+using Xamarin.Forms;
+using Concorsi.iOS;
+using Xamarin.Forms.Platform.iOS;
+using ImageCircle.Forms.Plugin.iOS;
+using static Concorsi.iOS.AppDelegate;
+using System.Threading.Tasks;
+using System.ComponentModel;
+using Xfx;
 
+[assembly: ExportRenderer(typeof(Button), typeof(MyButtonRenderer))]
 namespace Concorsi.iOS
 {
     // The UIApplicationDelegate for the application. This class is responsible for launching the 
@@ -20,12 +28,62 @@ namespace Concorsi.iOS
         //
         // You have 17 seconds to return from this method, or iOS will terminate your application.
         //
+        public override void OnResignActivation(UIApplication application)
+        {
+            var view = new UIView(Window.Frame)
+            {
+                Tag = new nint(101),
+                BackgroundColor = UIColor.White
+            };
+            Window.AddSubview(view);
+            Window.BringSubviewToFront(view);
+        }
+
+        // Remove window hiding app content when app is resumed
+        public override void OnActivated(UIApplication application)
+        {
+            var view = Window.ViewWithTag(new nint(101));
+            view?.RemoveFromSuperview();
+        }
+
         public override bool FinishedLaunching(UIApplication app, NSDictionary options)
         {
             global::Xamarin.Forms.Forms.Init();
-            LoadApplication(new App());
 
+            XfxControls.Init();
+
+            LoadApplication(new App());
+            ImageCircleRenderer.Init();
+            // Serve per non far andare l'applicazione in onSleep automaticamente
+            UIApplication.SharedApplication.IdleTimerDisabled = true;
             return base.FinishedLaunching(app, options);
+        }
+
+        public class MyButtonRenderer : ButtonRenderer
+        {
+            protected override void OnElementChanged(ElementChangedEventArgs<Button> e)
+            {
+                base.OnElementChanged(e);
+                if (Control != null)
+                {
+                    Control.TitleLabel.LineBreakMode = UIKit.UILineBreakMode.WordWrap;
+                    Control.TitleLabel.LineBreakMode = UILineBreakMode.CharacterWrap;
+                    Control.TitleLabel.TextAlignment = UITextAlignment.Center;
+                    Control.TitleLabel.Lines = 0;
+                }
+            }
+            protected override void OnElementPropertyChanged(object sender, PropertyChangedEventArgs e)
+            {
+                base.OnElementPropertyChanged(sender, e);
+                if (Control != null)
+                    Control.SetTitleColor(Color.White.ToUIColor(), UIControlState.Normal);
+                Control.SetTitleColor(Color.White.ToUIColor(), UIControlState.Application);
+                Control.SetTitleColor(Color.White.ToUIColor(), UIControlState.Disabled);
+                Control.SetTitleColor(Color.White.ToUIColor(), UIControlState.Focused);
+                Control.SetTitleColor(Color.White.ToUIColor(), UIControlState.Highlighted);
+                Control.SetTitleColor(Color.White.ToUIColor(), UIControlState.Selected);
+                Control.SetTitleColor(Color.White.ToUIColor(), UIControlState.Reserved);
+            }
         }
     }
 }
