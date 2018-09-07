39e08f14cb47fa75fd52949becf256b468a42f9a
ASvalente <antosaverio.valente@gmail.com>
2018-09-07 15:30:25 +0200

    aggiunte modifiche alla login

--- Concorsi/Concorsi/ModelView/LoginModelView.cs
+++ Concorsi/Concorsi/ModelView/LoginModelView.cs
@@ -12,7 +12,6 @@ using Concorsi.View;
 
 #endregion
 
-
 /*
  * Questa classe è il ModelView delle pagine Login e LoginIOS. La classe gestisce tutte le informazioni inserite e prelevate da remoto riguardanti l'utenza del cliente.
  * La classe concede l'accesso all'utente solo se le informazioni da lui inserite sono corrette. 
@@ -25,21 +24,19 @@ namespace Concorsi.ModelView
 
         #region DichiarazioneVariabili
 
-        private Utente utente; //Oggetto che astrae l'utenza del cliente e che nel caso in cui la login vada a buon fine conterrà le informazioni relative all'utente
         private bool flagLogin = false;//Booleano che andrà a true quando l'utente avrà effettuato la login
         public event PropertyChangedEventHandler PropertyChanged; //evento che implementa l'interfaccia INotifyPropertyChanged
         private string nameErrorTextPassword;//Variabile utilizzata nel caso in cui ci sia un errore nel campo password o sia vuoto
-        private Utenza utenteP;
+        private Utente utente;
         private bool isbusy; //variabile booleana utilizzata per gestire la proprietà IsRunning dell'activity indicator
         private string nameErrorText;//Variabile utilizzata nel caso in cui ci sia un errore nel campo nome o sia vuoto
         private bool showPassword = true;//Booleano utilizzato per mostrare o meno la password
         private bool isvisible; //variabile booleana utilizzata per gestire la proprietà IsVisible dell'activity indicator
         private bool loginisvisible;//Booleano utilizzato per mostrare o meno alcuni elementi nello xaml
-        private bool signupisvisible;//Booleano utilizzato per rendere visibile o meno la label per la registrazione
         private bool isenabled;//booleano utilizzato per abilitare o meno un elemento nello xaml
         private LoginPage loginPage;//Oggetto del tipo della pagina Login
         private List<Header> listaHeader = new List<Header>();//Lista di header
-        private ImageSource showPasswordImage = "EyePasswordWhite.png";//Sorgente da cui andremo a prendere l'immagine dell'occhio per mostrare la password
+        private ImageSource showPasswordImage = "eye.png";//Sorgente da cui andremo a prendere l'immagine dell'occhio per mostrare la password
 
         #endregion
 
@@ -131,22 +128,22 @@ namespace Concorsi.ModelView
         //Proprietà che definisce l' Username di chi effettua l'accesso
         public string Username
         {
-            get { return utente.getUserName; }
+            get { return GestioneUtente.Instance.getUserName; }
             set
             {
                 OnPropertyChanged();
-                utente.getUserName = value;
+                GestioneUtente.Instance.getUserName = value;
             }
         }
 
         //Proprietà che definisce la password di chi effettua il login
         public string passWord
         {
-            get { return utente.getPassword; }
+            get { return GestioneUtente.Instance.getPassword; }
             set
             {
                 OnPropertyChanged();
-                utente.getPassword = value;
+                GestioneUtente.Instance.getPassword = value;
             }
         }
 
@@ -178,10 +175,7 @@ namespace Concorsi.ModelView
         // Costruttore del ModelView che inizializza le variabili fondamentali per il corretto funzionamento della pagina di login (sia Android che IOS).
         public LoginModelView(LoginPage loginPage)
         {
-            utente = new Utente(); //Crea un oggetto Utente vuoto
-            utenteP = new Utenza();
-           
-            Username = utente.getUserName = utente.recuperaUserName();
+            utente = new Utente();
             LoginIsVisible = true;
             this.loginPage = loginPage;
             IsEnabled = true;
@@ -207,18 +201,15 @@ namespace Concorsi.ModelView
                     LoginIsVisible = false;
                     IsVisible = true; //L'activity indicator è visibile
                     IsBusy = true; //L'activity indicator è in stato IsRunning
-                    utenteP.username = Username;
-                    utenteP.password = passWord;
-                    utenteP.devDescrizione = utente.getDevDescrizione;
-                    utenteP.devInfo = utente.getDevInfo;
-                  await  utenteP.Login();
-                    /* utente.cancellaEdAggiornaUsername(utente.getUserName);
-                     App.Current.MainPage = new MainPage();*/
+                    utente.username = Username;
+                    utente.password = passWord;
+                    utente.devDescrizione = GestioneUtente.Instance.getDevDescrizione;
+                    utente.devInfo = GestioneUtente.Instance.getDevInfo;
+                    await  utente.Login();
                     IsBusy = false; //L'activity indicator non è in stato IsRunning
-                        IsVisible = false; //L'activity indicator non è visibile
-                        LoginIsVisible = true;
-                        IsEnabled = true;
-                    
+                    IsVisible = false; //L'activity indicator non è visibile
+                    LoginIsVisible = true;
+                    IsEnabled = true;
                 }
                 else
                     IsEnabled = true;
@@ -235,7 +226,7 @@ namespace Concorsi.ModelView
                 else
                 {
                     ShowPassword = true;
-                    ShowPasswordImage = "EyePasswordWhite.png";
+                    ShowPasswordImage = "eye_hide.png";
                 }
             });
         }
---
+++ Concorsi/Concorsi/Service/GestioneUtente.cs
@@ -0,0 +1,160 @@
+﻿using Plugin.DeviceInfo;
+using Xamarin.Forms;
+
+/*
+ * @Author: Antonio Saverio Valente
+ * 
+ * Questa classe serve a tenere traccia dei dati personali di accesso e 
+ * dei dati anagrafici dell'utente. Gestisce inoltre il salvataggio delle credenziali sul
+ * dispositivo definendo delle classi capaci di memorizzare le informazioni
+ * all'interno del dizionario dell'applicazione (Application.Current.Properties)
+ * */
+
+namespace Concorsi.Service
+{
+    public class GestioneUtente
+    {
+        // Indica l'username dell'utente
+        private string userName;
+        // Indica la Password dell'utente
+        private string password;
+        // Indica il nome di battesimo dell'utente
+        private string nomeDiBattesimo;
+        // Costituisce la dichiarazione di un oggetto di tipo Utente
+        private static GestioneUtente instance;
+        // Informazioni del device
+        private string devInfo = "";
+        // Descrizione del device utente
+        private string devDescrizione = "";
+        //Il bit di login assumerà valore 1 durante l'esecuzione della loginpage; 
+        // # FORSE VA TOLTO # 
+        private int bitLogin = 0;
+
+        /**
+         * 
+         * @param us: parametro che indica l'username, serve per inizializzare l'oggetto
+         * @param pass: è un parametro che viene passato per inizializzare la variabile di istanza password
+         * dell'oggetto Utente
+         * Il costruttore privato permette i generare un signleton della classe utente.
+         * 
+         * */
+        private GestioneUtente(string us, string pass)
+        {
+            this.userName = us;
+            this.password = pass;
+            this.devInfo = getDevInfo;
+            this.devDescrizione = getDevDescrizione;
+        }
+
+        public string getUserName { get; set; }
+        public string getPassword { get; set; }
+        public string getNomeDiBattesimo { get; set; }
+        public int getBitLogin { get; set; }
+
+        public string getDevInfo
+        {
+            get
+            {
+                devInfo = CrossDeviceInfo.Current.Id;
+                return devInfo;
+            }
+            set
+            {
+                devInfo = CrossDeviceInfo.Current.Id;
+            }
+        }
+
+        public string getDevDescrizione
+        {
+            get
+            {
+                if (Device.RuntimePlatform == Device.iOS)
+                {
+                    devDescrizione = CrossDeviceInfo.Current.Idiom + " " + CrossDeviceInfo.Current.Model + " " + CrossDeviceInfo.Current.Platform;
+                }
+                else if (Device.RuntimePlatform == Device.Android)
+                {
+                    devDescrizione = CrossDeviceInfo.Current.Idiom + " " + CrossDeviceInfo.Current.Model + " " + CrossDeviceInfo.Current.Platform;
+                }
+                return devDescrizione;
+            }
+            set
+            {
+                if (Device.RuntimePlatform == Device.iOS)
+                {
+                    devDescrizione = CrossDeviceInfo.Current.Idiom + " " + CrossDeviceInfo.Current.Model + " " + CrossDeviceInfo.Current.Platform;
+                }
+                else if (Device.RuntimePlatform == Device.Android)
+                {
+                    devDescrizione = CrossDeviceInfo.Current.Idiom + " " + CrossDeviceInfo.Current.Model + " " + CrossDeviceInfo.Current.Platform;
+                }
+            }
+        }
+
+        /**
+         * Questo metodo genera un singleton della classe Utente
+         * */
+        public static GestioneUtente Instance
+        {
+            get
+            {
+                if (instance == null)
+                {
+                    instance = new GestioneUtente("", "");
+                }
+                return instance;
+            }
+        }
+
+        /**
+         * Questo metodo permette di salvare le credenziali (in particolare l'username) dell'utente
+         * all'interno del dizionario gestito dal metodo statico Application.Current.Properties.Add
+         * */
+        public void salvaCredenzialiAccesso(string user)
+        {
+            if (!string.IsNullOrWhiteSpace(user))
+            {
+                this.userName = user;
+                Application.Current.Properties.Add("userName", user);
+                Application.Current.SavePropertiesAsync();
+            }
+        }
+
+        /**
+         * Questo metodo permette di recuperare l'username utente dalla memoria interna.
+         * */
+        public string recuperaUserName()
+        {
+            string u;
+            /**
+             * Se è già presente una chiave userName in memoria viene recuperato il campo userName 
+             * dal dizionario interno dell'applicazione che è stato in precedenza settatto 
+             * al valore inserito in fase di login. 
+             * Tale dizionario (Application.Current.Properties) viene usato per memorizzare i dati
+             * sul dispositivo.
+             * */
+            if (Application.Current.Properties.ContainsKey("userName"))
+            {
+                u = Application.Current.Properties["userName"].ToString();
+            }
+            else
+            {
+                u = null;
+            }
+            return u;
+        }
+
+        /**
+         * Questo metodo agisce in caso di aggiornamento da parte dell'utente dell'username.
+         * In tal modo viene salvato il nuovo username inserito in memoria del telefono, in 
+         * particolare all'interno dell'Application.Current...
+         * */
+        public void cancellaEdAggiornaUsername(string nuovo)
+        {
+            this.userName = nuovo;
+            Application.Current.Properties.Clear();
+            Application.Current.Properties.Add("userName", nuovo);
+            Application.Current.SavePropertiesAsync();
+        }
+    }
+}
