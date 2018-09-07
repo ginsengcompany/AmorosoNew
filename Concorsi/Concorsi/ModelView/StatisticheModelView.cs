3d857c0fe5909c1662a819053407d8b301cb5afd
ASvalente <antosaverio.valente@gmail.com>
2018-09-07 09:35:18 +0200

    aggiunta classe statistiche di esempio

---
+++ Concorsi/Concorsi/ModelView/StatisticheModelView.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Concorsi.ModelView
+{
+    class StatisticheModelView
+    {
+    }
+}
---
+++ Concorsi/Concorsi/View/Statistiche.xaml.cs
@@ -0,0 +1,39 @@
+﻿using Concorsi.ModelView;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Concorsi.View
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Statistiche : ContentPage
+    {
+        private StatisticheModelView modelView;
+
+        public Statistiche()
+        {
+            InitializeComponent();
+            modelView = new StatisticheModelView();
+            BindingContext = modelView;
+        }
+
+        private void Picker_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            Picker picker = sender as Picker;
+            if (picker.SelectedIndex > 0)
+            {
+                string materia = picker.SelectedItem as string;
+                modelView.getStatistiche(materia);
+            }
+            else if (picker.SelectedIndex == 0)
+            {
+                modelView.getStatistiche("");
+            }
+        }
+    }
+}
