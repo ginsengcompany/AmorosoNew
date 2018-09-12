5ae9c9b8f215a4bbe99e84c62f310821a715d890
ASvalente <antosaverio.valente@gmail.com>
2018-09-12 11:51:03 +0200

    aggiungo modifiche ai layout di cronologia e di main page

--- Concorsi/Concorsi/View/MainPage.xaml.cs
+++ Concorsi/Concorsi/View/MainPage.xaml.cs
@@ -14,6 +14,8 @@ namespace Concorsi
             InitializeComponent();
             labelReminder.Text = "Gentile " + utente.nome + " ti invitiamo ad effettuare l'operazione di Log Out " +
                 "al termine dell'utilizzo dell'applicazione.";
+            labelReminder.Style = Device.Styles.TitleStyle;
+            labelReminder.TextColor = Color.FromHex("1b2776");
             z = new MainPageModelView(this, utente);
             BindingContext = z; //Questa pagina utilizza l'MWWM ed effettua il binding con la classe LoginModelView
 
