3379d833b646648961ea4c2afebc70a096d92988
AlessioRivoli <pleyboy87@hotmail.it>
2018-09-13 16:50:48 +0200

    modificato nome apprendimentodetailsPage e iniziato a costruirla

---
+++ Concorsi/Concorsi/View/ApprendimentoDetailsPage.xaml.cs
@@ -0,0 +1,24 @@
+﻿using Concorsi.ModelView;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Concorsi.View
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class ApprendimentoDetailsPage : ContentPage
+    {
+        public ApprendimentoDetailsPageModelView listaArgomentiVideolezioni;
+
+        public ApprendimentoDetailsPage()
+        {
+            InitializeComponent();
+            listaArgomentiVideolezioni = new ApprendimentoDetailsPageModelView();
+            BindingContext = listaArgomentiVideolezioni;
+        }
+	}
+}
