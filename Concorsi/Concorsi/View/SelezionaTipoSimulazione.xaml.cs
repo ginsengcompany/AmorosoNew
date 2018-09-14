42e53a17d3a6325fb7ffeed18255c0332de4124a
FabriGinseng <antoniofabriziofiume@libero.it>
2018-09-14 14:21:01 +0200

    aggiunte pagine quiz, simulazione e modalita classica

---
+++ Concorsi/Concorsi/View/SelezionaTipoSimulazione.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Concorsi.View
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class SelezionaTipoSimulazione : ContentPage
+	{
+		public SelezionaTipoSimulazione ()
+		{
+			InitializeComponent ();
+		}
+	}
+}
