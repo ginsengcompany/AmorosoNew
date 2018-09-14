57d6aaadcdbf19eb6564b363e2451b8b3f7f0bcb
FabriGinseng <antoniofabriziofiume@libero.it>
2018-09-14 16:28:36 +0200

    aggiunte card

--- Concorsi/Concorsi/ModelView/SelezionaModalitaQuizPageModelView.cs
+++ Concorsi/Concorsi/ModelView/SelezionaModalitaQuizPageModelView.cs
@@ -49,13 +49,8 @@ namespace Concorsi.ModelView
 
         public SelezionaModalitaQuizPageModelView()
         {
-            TestoModalitaClassica= "In questa modalità di esercitazione avrai la possibilità " +
-                                   "di eseguire una Simulazione d'esame sia in modalità Classica che in modalità Assistita. " +
-                                   "Entrambe le modalità prevedono che selezionata una risposta, automaticamente si passa alla successiva." +
-                                   "La Simulazione Assistita rispetto alla modalità Simulazione Classica permette" +
-                                   "in caso di errore di vedere indicata la risposta esatta." +
-                                   "Nota: premendo il tasto back in alto a sinistra le statistiche saranno contate." +
-                                   "In questa modalità è previsto una misura cronometrica dei tempi di risposta";
+            TestoModalitaClassica = "In questa modalità di esercitazione avrai la possibilità " +
+                                    "di eseguire una Simulazione d'esame sia in modalità Classica che in modalità Assistita. ";
             TestoModalitaVeloce= "In questa modalità avrai la possibilità di eseguire un test sfruttando le " +
                                  "domande presenti nell'intera banca dati di Amoroso Concorsi. Selezionando le opzioni in maniera opportuna " +
                                  "avrai la possibilità di definire il tipo di esercitazione da svolgere in maniera mirata." +
