6869e30c8327abc3bcbb7091fd12e9331376cca6
FabriGinseng <antoniofabriziofiume@libero.it>
2018-09-17 11:40:00 +0200

    aggiunto model view a modalita classica

--- Concorsi/Concorsi/View/ModalitaClassicaPage.xaml.cs
+++ Concorsi/Concorsi/View/ModalitaClassicaPage.xaml.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using Concorsi.ModelView;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
@@ -12,9 +12,12 @@ namespace Concorsi.View
 	[XamlCompilation(XamlCompilationOptions.Compile)]
 	public partial class ModalitaClassicaPage : ContentPage
 	{
+	    private ModalitaClassicaPageModelView modelView;
 		public ModalitaClassicaPage ()
 		{
 			InitializeComponent ();
+            modelView= new ModalitaClassicaPageModelView();
+		    BindingContext = modelView;
 		}
 	}
 }
