d261f1d9f981c4b6ad3b5683e77e0ec8ffffa2f1
FabriGinseng <antoniofabriziofiume@libero.it>
2018-09-19 16:48:31 +0200

    aggiunta classe concorso

---
+++ Concorsi/Concorsi/Model/Concorso.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Concorsi.Model
+{
+    public class Concorso
+    { 
+       
+            // denominazione per esteso del concorso (Es. Esercito Marina Aeronautica", Corpo json)
+            public string Corpo { get; set; }
+            // identificativo numerico del concorso (Es. 5, id_concorso json)
+            public string id_concorso { get; set; }
+            // codice alfanumerico del concorso (Es. VFP4, codice_concorso json) 
+            public string codice_concorso { get; set; }
+            // punteggio associato alla risposta esatta (rispostaesatta json)
+            public string rispostaesatta { get; set; }
+            // punteggio associato alla risposta errata (rispostaerrata json)
+            public string rispostaerrata { get; set; }
+            // numedo delle domande contenute in una prova di tipo quiz veloce (numerodomande json)
+            public string numerodomande { get; set; }
+            // numero totale delle domande contenute nell'intero concorso (domandemax json)
+            public string domandemax { get; set; }
+        
+
+    }
+}
